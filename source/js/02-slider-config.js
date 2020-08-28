0011696d0e352b094785fe989b0580259c7c639a
kosweb <koleitdev@gmail.com>
2020-08-28 16:17:38 +0300

    madslides

--- source/js/02-slider-config.js
+++ source/js/02-slider-config.js
@@ -1,78 +1,105 @@
-const headerSlider = document.querySelector('.page-header__slider-container');
-const extendedSlider = document.querySelector('.extended-films__slider-container');
-const digitalSlider = document.querySelector('.digital-release__slider-container');
-const famousSlider = document.querySelector('.famous-movies__slider-container');
-const newsSlider = document.querySelector('.movie-news__slider-container');
+// const headerSlider = document.querySelector('.page-header__slider-container');
+// const extendedSlider = document.querySelector('.extended-films__slider-container');
+// const digitalSlider = document.querySelector('.digital-release__slider-container');
+// const famousSlider = document.querySelector('.famous-movies__slider-container');
+// const newsSlider = document.querySelector('.movie-news__slider-container');
 
-let mySwiperHead = new Swiper(headerSlider, {
-	slidesPerView: 1,
-	loop: true,
-	effect: 'fade',
-  speed: 500,
-	// autoplay: {
-	// 	delay: 2000,
-  // },
-})
 
-let mySwiperEx = new Swiper(extendedSlider, {
-  slidesPerView: 'auto',
-  spaceBetween: 0,
-  cssMode: true,
-  loop: false,
+const headerSlider = new Splide('.page-header__slider-container', {
+  pagination: false,
+  perPage: 1,
+  type: 'fade',
+  arrows: false,
+  drag: false,
+  // autoplay: true,
+  interval: 3000,
+  speed: 1000,
+  rewind: true,
+}).mount();
+
+
+const exSlider = new Splide('.extended-films__slider', {
+  rewind : true,
+  pagination: false,
+  gap: 40,
+  perPage: 3,
+  perMove: 1,
+  padding: {
+    left : 150,
+    right: 150,
+  },
 
   breakpoints: {
-    1280: {
-      cssMode: false,
-      slidesPerView: 5,
-      loop: true,
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    },
-  }
-})
+		1280: {
+     destroy: true,
+		},
+  },
+}).mount();
 
-let mySwiperDig = new Swiper(digitalSlider, {
+
+const digitalSlider = new Splide('.digital-release__slider', {
+  rewind : true,
+  pagination: false,
+  gap: 0,
+  perPage: 7,
+  perMove: 1,
+  padding: {
+    left : 150,
+    right: 150,
+  },
+
   breakpoints: {
-    1280: {
-      slidesPerView: 5,
-      loop: true,
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    },
+		1280: {
+     destroy: true,
+		},
+  },
+}).mount();
+
+
+// const fmsSlider = new Splide('.famous-movies__slider-container', {
+//   rewind : true,
+//   pagination: false,
+//   gap: 20,
+//   perPage: 2,
+//   perMove: 1,
+//   padding: {
+//     left : 150,
+//     right: 150,
+//   },
+
+//   // breakpoints: {
+// 	// 	1280: {
+//   //    destroy: true,
+// 	// 	},
+//   // },
+// }).mount();
+
+
+exSlider.on( 'moved', function(newIndex, oldIndex, destIndex) {
+  const edge = exSlider.Components.Controller.edgeIndex;
+
+  console.log(`oldIndex ${oldIndex} | newIndex ${newIndex} | destIndex ${destIndex} | edge ${edge}`);
+
+  if ( newIndex == 0 && oldIndex == 0 && destIndex == 0 ) {
+    exSlider.go( '<' );
   }
-})
 
-let mySwiperFms = new Swiper(famousSlider, {
-	slidesPerView: 1,
-	spaceBetween: 20,
-	loop: true,
-	pagination: {
-    el: '.famous-pagination',
-		type: 'bullets',
-		clickable: true,
-	},
-	// navigation: {
-  //   nextEl: '.swiper-button-next',
-  //   prevEl: '.swiper-button-prev',
-	// },
-})
+  if ( oldIndex == edge && destIndex == edge ) {
+    exSlider.go( '>' );
+  }
+});
 
 
-let mySwiperNews = new Swiper(newsSlider, {
-	slidesPerView: 1,
-	spaceBetween: 20,
-	loop: true,
-	pagination: {
-    el: '.news-pagination',
-		type: 'fraction',
-	},
-})
+// digitalSlider.on( 'moved', function(newIndex, oldIndex, destIndex) {
+//   const edge = digitalSlider.Components.Controller.edgeIndex;
 
+//   console.log(`oldIndex ${oldIndex} | newIndex ${newIndex} | destIndex ${destIndex} | edge ${edge}`);
 
+//   if ( newIndex == 0 && oldIndex == 0 && destIndex == 0 ) {
+//     digitalSlider.go( '<' );
+//   }
 
-
-// GLIDER CONFIG
+//   if ( oldIndex == edge && destIndex == edge ) {
+//     digitalSlider.go( '>' );
+//   }
+// });
--- source/js/05-modal.js
+++ source/js/05-modal.js
@@ -1,226 +1,226 @@
 // EXTEND VIDEO BLOCK
-const body = document.querySelector('body');
+// const body = document.querySelector('body');
 
 // FAMOUS VIDEO BLOCK
 
 // VIDEO 1
 
-let fmsTrailer1 = document.querySelector('.famous-movies__trailer-play-1');
-let fmsModal1 = document.querySelector('.modal__famous-1');
-let fmsModalVideo1 = document.querySelector('.fms-modal__video-1');
-let fmsModalClose1 = document.querySelector('.fms-modal__close-1');
+// let fmsTrailer1 = document.querySelector('.famous-movies__trailer-play-1');
+// let fmsModal1 = document.querySelector('.modal__famous-1');
+// let fmsModalVideo1 = document.querySelector('.fms-modal__video-1');
+// let fmsModalClose1 = document.querySelector('.fms-modal__close-1');
 
-fmsTrailer1.addEventListener('click', function(evt) {
-  evt.preventDefault();
-	fmsModal1.classList.add('modal__show');
-	body.style.overflow = 'hidden';
-	fmsModalVideo1.autoplay = true;
-	fmsModalVideo1.load();
-});
+// fmsTrailer1.addEventListener('click', function(evt) {
+//   evt.preventDefault();
+// 	fmsModal1.classList.add('modal__show');
+// 	body.style.overflow = 'hidden';
+// 	fmsModalVideo1.autoplay = true;
+// 	fmsModalVideo1.load();
+// });
 
-window.addEventListener('keydown', function(evt) {
-  if (evt.keyCode === 27) {
-    if (fmsModal1.classList.contains('modal__show')) {
-      evt.preventDefault();
-			fmsModal1.classList.remove('modal__show');
-			body.style.overflow = '';
-			fmsModalVideo1.autoplay = false;
-			fmsModalVideo1.load();
-    }
-  }
-});
+// window.addEventListener('keydown', function(evt) {
+//   if (evt.keyCode === 27) {
+//     if (fmsModal1.classList.contains('modal__show')) {
+//       evt.preventDefault();
+// 			fmsModal1.classList.remove('modal__show');
+// 			body.style.overflow = '';
+// 			fmsModalVideo1.autoplay = false;
+// 			fmsModalVideo1.load();
+//     }
+//   }
+// });
 
-fmsModalClose1.addEventListener('click', function(evt) {
-	if (fmsModal1.classList.contains('modal__show')) {
-		evt.preventDefault();
-		fmsModal1.classList.remove('modal__show');
-		body.style.overflow = '';
-		fmsModalVideo1.autoplay = false;
-		fmsModalVideo1.load();
-	}
-});
+// fmsModalClose1.addEventListener('click', function(evt) {
+// 	if (fmsModal1.classList.contains('modal__show')) {
+// 		evt.preventDefault();
+// 		fmsModal1.classList.remove('modal__show');
+// 		body.style.overflow = '';
+// 		fmsModalVideo1.autoplay = false;
+// 		fmsModalVideo1.load();
+// 	}
+// });
 
-// VIDEO 2
+// // VIDEO 2
 
-let fmsTrailer2 = document.querySelector('.famous-movies__trailer-play-2');
-let fmsModal2 = document.querySelector('.modal__famous-2');
-let fmsModalVideo2 = document.querySelector('.fms-modal__video-2');
-let fmsModalClose2 = document.querySelector('.fms-modal__close-2');
+// let fmsTrailer2 = document.querySelector('.famous-movies__trailer-play-2');
+// let fmsModal2 = document.querySelector('.modal__famous-2');
+// let fmsModalVideo2 = document.querySelector('.fms-modal__video-2');
+// let fmsModalClose2 = document.querySelector('.fms-modal__close-2');
 
-fmsTrailer2.addEventListener('click', function(evt) {
-  evt.preventDefault();
-	fmsModal2.classList.add('modal__show');
-	body.style.overflow = 'hidden';
-	fmsModalVideo2.autoplay = true;
-	fmsModalVideo2.load();
-});
+// fmsTrailer2.addEventListener('click', function(evt) {
+//   evt.preventDefault();
+// 	fmsModal2.classList.add('modal__show');
+// 	body.style.overflow = 'hidden';
+// 	fmsModalVideo2.autoplay = true;
+// 	fmsModalVideo2.load();
+// });
 
-window.addEventListener('keydown', function(evt) {
-  if (evt.keyCode === 27) {
-    if (fmsModal2.classList.contains('modal__show')) {
-      evt.preventDefault();
-			fmsModal2.classList.remove('modal__show');
-			body.style.overflow = '';
-			fmsModalVideo2.autoplay = false;
-			fmsModalVideo2.load();
-    }
-  }
-});
+// window.addEventListener('keydown', function(evt) {
+//   if (evt.keyCode === 27) {
+//     if (fmsModal2.classList.contains('modal__show')) {
+//       evt.preventDefault();
+// 			fmsModal2.classList.remove('modal__show');
+// 			body.style.overflow = '';
+// 			fmsModalVideo2.autoplay = false;
+// 			fmsModalVideo2.load();
+//     }
+//   }
+// });
 
-fmsModalClose2.addEventListener('click', function(evt) {
-	if (fmsModal2.classList.contains('modal__show')) {
-		evt.preventDefault();
-		fmsModal2.classList.remove('modal__show');
-		body.style.overflow = '';
-		fmsModalVideo2.autoplay = false;
-		fmsModalVideo2.load();
-	}
-});
+// fmsModalClose2.addEventListener('click', function(evt) {
+// 	if (fmsModal2.classList.contains('modal__show')) {
+// 		evt.preventDefault();
+// 		fmsModal2.classList.remove('modal__show');
+// 		body.style.overflow = '';
+// 		fmsModalVideo2.autoplay = false;
+// 		fmsModalVideo2.load();
+// 	}
+// });
 
-// VIDEO 3
+// // VIDEO 3
 
-let fmsTrailer3 = document.querySelector('.famous-movies__trailer-play-3');
-let fmsModal3 = document.querySelector('.modal__famous-3');
-let fmsModalVideo3 = document.querySelector('.fms-modal__video-3');
-let fmsModalClose3 = document.querySelector('.fms-modal__close-3');
+// let fmsTrailer3 = document.querySelector('.famous-movies__trailer-play-3');
+// let fmsModal3 = document.querySelector('.modal__famous-3');
+// let fmsModalVideo3 = document.querySelector('.fms-modal__video-3');
+// let fmsModalClose3 = document.querySelector('.fms-modal__close-3');
 
-fmsTrailer3.addEventListener('click', function(evt) {
-  evt.preventDefault();
-	fmsModal3.classList.add('modal__show');
-	body.style.overflow = 'hidden';
-	fmsModalVideo3.autoplay = true;
-	fmsModalVideo3.load();
-});
+// fmsTrailer3.addEventListener('click', function(evt) {
+//   evt.preventDefault();
+// 	fmsModal3.classList.add('modal__show');
+// 	body.style.overflow = 'hidden';
+// 	fmsModalVideo3.autoplay = true;
+// 	fmsModalVideo3.load();
+// });
 
-window.addEventListener('keydown', function(evt) {
-  if (evt.keyCode === 27) {
-    if (fmsModal3.classList.contains('modal__show')) {
-      evt.preventDefault();
-			fmsModal3.classList.remove('modal__show');
-			body.style.overflow = '';
-			fmsModalVideo3.autoplay = false;
-			fmsModalVideo3.load();
-    }
-  }
-});
+// window.addEventListener('keydown', function(evt) {
+//   if (evt.keyCode === 27) {
+//     if (fmsModal3.classList.contains('modal__show')) {
+//       evt.preventDefault();
+// 			fmsModal3.classList.remove('modal__show');
+// 			body.style.overflow = '';
+// 			fmsModalVideo3.autoplay = false;
+// 			fmsModalVideo3.load();
+//     }
+//   }
+// });
 
-fmsModalClose3.addEventListener('click', function(evt) {
-	if (fmsModal3.classList.contains('modal__show')) {
-		evt.preventDefault();
-		fmsModal3.classList.remove('modal__show');
-		body.style.overflow = '';
-		fmsModalVideo3.autoplay = false;
-		fmsModalVideo3.load();
-	}
-});
+// fmsModalClose3.addEventListener('click', function(evt) {
+// 	if (fmsModal3.classList.contains('modal__show')) {
+// 		evt.preventDefault();
+// 		fmsModal3.classList.remove('modal__show');
+// 		body.style.overflow = '';
+// 		fmsModalVideo3.autoplay = false;
+// 		fmsModalVideo3.load();
+// 	}
+// });
 
-// VIDEO 4
+// // VIDEO 4
 
-let fmsTrailer4 = document.querySelector('.famous-movies__trailer-play-4');
-let fmsModal4 = document.querySelector('.modal__famous-4');
-let fmsModalVideo4 = document.querySelector('.fms-modal__video-4');
-let fmsModalClose4 = document.querySelector('.fms-modal__close-4');
+// let fmsTrailer4 = document.querySelector('.famous-movies__trailer-play-4');
+// let fmsModal4 = document.querySelector('.modal__famous-4');
+// let fmsModalVideo4 = document.querySelector('.fms-modal__video-4');
+// let fmsModalClose4 = document.querySelector('.fms-modal__close-4');
 
-fmsTrailer4.addEventListener('click', function(evt) {
-  evt.preventDefault();
-	fmsModal4.classList.add('modal__show');
-	body.style.overflow = 'hidden';
-	fmsModalVideo4.autoplay = true;
-	fmsModalVideo4.load();
-});
+// fmsTrailer4.addEventListener('click', function(evt) {
+//   evt.preventDefault();
+// 	fmsModal4.classList.add('modal__show');
+// 	body.style.overflow = 'hidden';
+// 	fmsModalVideo4.autoplay = true;
+// 	fmsModalVideo4.load();
+// });
 
-window.addEventListener('keydown', function(evt) {
-  if (evt.keyCode === 27) {
-    if (fmsModal4.classList.contains('modal__show')) {
-      evt.preventDefault();
-			fmsModal4.classList.remove('modal__show');
-			body.style.overflow = '';
-			fmsModalVideo4.autoplay = false;
-			fmsModalVideo4.load();
-    }
-  }
-});
+// window.addEventListener('keydown', function(evt) {
+//   if (evt.keyCode === 27) {
+//     if (fmsModal4.classList.contains('modal__show')) {
+//       evt.preventDefault();
+// 			fmsModal4.classList.remove('modal__show');
+// 			body.style.overflow = '';
+// 			fmsModalVideo4.autoplay = false;
+// 			fmsModalVideo4.load();
+//     }
+//   }
+// });
 
-fmsModalClose4.addEventListener('click', function(evt) {
-	if (fmsModal4.classList.contains('modal__show')) {
-		evt.preventDefault();
-		fmsModal4.classList.remove('modal__show');
-		body.style.overflow = '';
-		fmsModalVideo4.autoplay = false;
-		fmsModalVideo4.load();
-	}
-});
+// fmsModalClose4.addEventListener('click', function(evt) {
+// 	if (fmsModal4.classList.contains('modal__show')) {
+// 		evt.preventDefault();
+// 		fmsModal4.classList.remove('modal__show');
+// 		body.style.overflow = '';
+// 		fmsModalVideo4.autoplay = false;
+// 		fmsModalVideo4.load();
+// 	}
+// });
 
-// VIDEO 5
+// // VIDEO 5
 
-let fmsTrailer5 = document.querySelector('.famous-movies__trailer-play-5');
-let fmsModal5 = document.querySelector('.modal__famous-5');
-let fmsModalVideo5 = document.querySelector('.fms-modal__video-5');
-let fmsModalClose5 = document.querySelector('.fms-modal__close-5');
+// let fmsTrailer5 = document.querySelector('.famous-movies__trailer-play-5');
+// let fmsModal5 = document.querySelector('.modal__famous-5');
+// let fmsModalVideo5 = document.querySelector('.fms-modal__video-5');
+// let fmsModalClose5 = document.querySelector('.fms-modal__close-5');
 
-fmsTrailer5.addEventListener('click', function(evt) {
-  evt.preventDefault();
-	fmsModal5.classList.add('modal__show');
-	body.style.overflow = 'hidden';
-	fmsModalVideo5.autoplay = true;
-	fmsModalVideo5.load();
-});
+// fmsTrailer5.addEventListener('click', function(evt) {
+//   evt.preventDefault();
+// 	fmsModal5.classList.add('modal__show');
+// 	body.style.overflow = 'hidden';
+// 	fmsModalVideo5.autoplay = true;
+// 	fmsModalVideo5.load();
+// });
 
-window.addEventListener('keydown', function(evt) {
-  if (evt.keyCode === 27) {
-    if (fmsModal5.classList.contains('modal__show')) {
-      evt.preventDefault();
-			fmsModal5.classList.remove('modal__show');
-			body.style.overflow = '';
-			fmsModalVideo5.autoplay = false;
-			fmsModalVideo5.load();
-    }
-  }
-});
+// window.addEventListener('keydown', function(evt) {
+//   if (evt.keyCode === 27) {
+//     if (fmsModal5.classList.contains('modal__show')) {
+//       evt.preventDefault();
+// 			fmsModal5.classList.remove('modal__show');
+// 			body.style.overflow = '';
+// 			fmsModalVideo5.autoplay = false;
+// 			fmsModalVideo5.load();
+//     }
+//   }
+// });
 
-fmsModalClose5.addEventListener('click', function(evt) {
-	if (fmsModal5.classList.contains('modal__show')) {
-		evt.preventDefault();
-		fmsModal5.classList.remove('modal__show');
-		body.style.overflow = '';
-		fmsModalVideo5.autoplay = false;
-		fmsModalVideo5.load();
-	}
-});
+// fmsModalClose5.addEventListener('click', function(evt) {
+// 	if (fmsModal5.classList.contains('modal__show')) {
+// 		evt.preventDefault();
+// 		fmsModal5.classList.remove('modal__show');
+// 		body.style.overflow = '';
+// 		fmsModalVideo5.autoplay = false;
+// 		fmsModalVideo5.load();
+// 	}
+// });
 
-// VIDEO 6
+// // VIDEO 6
 
-let fmsTrailer6 = document.querySelector('.famous-movies__trailer-play-6');
-let fmsModal6 = document.querySelector('.modal__famous-6');
-let fmsModalVideo6 = document.querySelector('.fms-modal__video-6');
-let fmsModalClose6 = document.querySelector('.fms-modal__close-6');
+// let fmsTrailer6 = document.querySelector('.famous-movies__trailer-play-6');
+// let fmsModal6 = document.querySelector('.modal__famous-6');
+// let fmsModalVideo6 = document.querySelector('.fms-modal__video-6');
+// let fmsModalClose6 = document.querySelector('.fms-modal__close-6');
 
-fmsTrailer6.addEventListener('click', function(evt) {
-  evt.preventDefault();
-	fmsModal6.classList.add('modal__show');
-	body.style.overflow = 'hidden';
-	fmsModalVideo6.autoplay = true;
-	fmsModalVideo6.load();
-});
+// fmsTrailer6.addEventListener('click', function(evt) {
+//   evt.preventDefault();
+// 	fmsModal6.classList.add('modal__show');
+// 	body.style.overflow = 'hidden';
+// 	fmsModalVideo6.autoplay = true;
+// 	fmsModalVideo6.load();
+// });
 
-window.addEventListener('keydown', function(evt) {
-  if (evt.keyCode === 27) {
-    if (fmsModal6.classList.contains('modal__show')) {
-      evt.preventDefault();
-			fmsModal6.classList.remove('modal__show');
-			body.style.overflow = '';
-			fmsModalVideo6.autoplay = false;
-			fmsModalVideo6.load();
-    }
-  }
-});
+// window.addEventListener('keydown', function(evt) {
+//   if (evt.keyCode === 27) {
+//     if (fmsModal6.classList.contains('modal__show')) {
+//       evt.preventDefault();
+// 			fmsModal6.classList.remove('modal__show');
+// 			body.style.overflow = '';
+// 			fmsModalVideo6.autoplay = false;
+// 			fmsModalVideo6.load();
+//     }
+//   }
+// });
 
-fmsModalClose6.addEventListener('click', function(evt) {
-	if (fmsModal6.classList.contains('modal__show')) {
-		evt.preventDefault();
-		fmsModal6.classList.remove('modal__show');
-		body.style.overflow = '';
-		fmsModalVideo6.autoplay = false;
-		fmsModalVideo6.load();
-	}
-});
+// fmsModalClose6.addEventListener('click', function(evt) {
+// 	if (fmsModal6.classList.contains('modal__show')) {
+// 		evt.preventDefault();
+// 		fmsModal6.classList.remove('modal__show');
+// 		body.style.overflow = '';
+// 		fmsModalVideo6.autoplay = false;
+// 		fmsModalVideo6.load();
+// 	}
+// });
